de775a75185e1a92bc7a72b05a7ca73c4f1c50b5
say ale <145525019+alekhinexxx@users.noreply.github.com>
2024-03-11 02:24:23 +0700

    Create If_Statement.py

---
+++ If_Statement.py
@@ -0,0 +1,11 @@
+#Menampilkan Judul Program
+print("Program Kualifikasi umur")
+
+#Variabel umur harus berisi Interger
+#kita Konversi dari String ke Integer
+umur = int(input("Masukan Umur Anda :"))
+print("Umur anda adalah : , ", umur)
+if age >=18:
+  print("Anda sudah Baligh")
+else:
+    print("Anda belum Baligh")
